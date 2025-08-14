c8a16dc4c8b5d2df2d5aa05324f8f41de3c264ff
IntelliRX <IntelliRX@users.noreply.github.com>
2025-08-14 04:03:13 +0000

    deploy: 73b67aee51287a0da9bad1438070da8b37fff354

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "63457511e9d796e3aec73443a51409d8",
+const RESOURCES = {"flutter_bootstrap.js": "ee40abe7bcfee552b73949cb25a4b90a",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
