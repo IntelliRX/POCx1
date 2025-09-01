812e6c6d2e257050c292c40a8817c2cd8a977158
IntelliRX <IntelliRX@users.noreply.github.com>
2025-09-01 03:17:14 +0000

    deploy: 73b67aee51287a0da9bad1438070da8b37fff354

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1782673225"
+    serviceWorkerVersion: "1735955507"
   }
 });
