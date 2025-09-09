cf7c8f4a54f0c11a952f2ed70ae991bc3e749966
IntelliRX <IntelliRX@users.noreply.github.com>
2025-09-09 02:57:53 +0000

    deploy: 73b67aee51287a0da9bad1438070da8b37fff354

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
 "assets/AssetManifest.bin.json": "589de7c5487e4ebe2d9b7a2886fd6f6f",
 "assets/AssetManifest.bin": "81f0999cc8faa0587bf90fbbdfdd6bb1",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"flutter_bootstrap.js": "15fd2ed785a3addb736a9749d39d1360",
+"flutter_bootstrap.js": "9ec87eda58c6c43268984a635c0103b5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "9202eef73339592e53571c6871ee5dbf",
 "/": "9202eef73339592e53571c6871ee5dbf"};
